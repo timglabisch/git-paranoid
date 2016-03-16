fa92cff5bf5656ba11d3fd5631380b0d2a309f47
timglabisch <tim@ainfach.de>
2016-03-16 22:13:20 +0100

    blllaaaabbb

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,9 @@
 extern crate git2;
 
 use git2::Repository;
+use git2::DiffFormat;
+use git2::DiffOptions;
+use std::str;
 
 fn main() {
 
@@ -28,6 +31,25 @@ fn main() {
         let commit = repo.find_commit(oid).unwrap();
 
         println!("{}", commit.message().unwrap());
+
+        let a = commit.tree().unwrap();
+        let b = if commit.parents().len() == 1 {
+            let parent = commit.parent(0).unwrap();
+            parent.tree().unwrap()
+        } else {
+            panic!("b is wrong");
+        };
+
+        let (mut diffopts, mut diffopts2) = (DiffOptions::new(), DiffOptions::new());
+        let diff = repo.diff_tree_to_tree(Some(&a), Some(&b), Some(&mut diffopts2)).unwrap();
+        diff.print(DiffFormat::Patch, |_delta, _hunk, line| {
+            match line.origin() {
+                ' ' | '+' | '-' => print!("{}", line.origin()),
+                _ => {}
+            }
+            print!("{}", str::from_utf8(line.content()).unwrap());
+            true
+        });
     }
 
     println!("Hello, world!");
