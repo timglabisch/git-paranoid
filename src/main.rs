ddddb66be83b0d9abc31ab9fccf62a44d6983a77
timglabisch <tim@ainfach.de>
2016-03-18 16:16:36 +0100

    store commits

--- src/main.rs
+++ src/main.rs
@@ -4,18 +4,26 @@ use git2::Repository;
 use git2::DiffFormat;
 use git2::DiffOptions;
 use std::str;
+use std::collections::HashMap;
+
 
 fn main() {
+
+    let mut found_commits = HashMap::new();
 
     let repo = match Repository::open("./") {
         Ok(repo) => repo,
         Err(e) => panic!("failed to open: {}", e),
     };
 
+    // 1. collect commit id's to analyze and save it's branches
+    // 2. analyze commits
+    // toml
+
     for b in repo.branches(None).unwrap() {
 
         let branch = b.unwrap();
-        let branch_name = branch.0.name().unwrap().unwrap();
+        let branch_name = branch.0.name().unwrap().unwrap().to_string();
 
         println!("{}", branch_name);
 
@@ -30,6 +38,12 @@ fn main() {
 
         let commit = repo.find_commit(oid).unwrap();
 
+        if !found_commits.contains_key(&oid) {
+            found_commits.insert(oid, vec![branch_name.clone()]);
+        } else {
+            found_commits.get_mut(&oid).unwrap().push(branch_name.clone());
+        }
+
         println!("{}", commit.message().unwrap());
 
         let a = commit.tree().unwrap();
@@ -40,8 +54,8 @@ fn main() {
             panic!("b is wrong");
         };
 
-        let (mut diffopts, mut diffopts2) = (DiffOptions::new(), DiffOptions::new());
-        let diff = repo.diff_tree_to_tree(Some(&a), Some(&b), Some(&mut diffopts2)).unwrap();
+        let mut diffopts = DiffOptions::new();
+        let diff = repo.diff_tree_to_tree(Some(&a), Some(&b), Some(&mut diffopts)).unwrap();
         diff.print(DiffFormat::Patch, |_delta, _hunk, line| {
             match line.origin() {
                 ' ' | '+' | '-' => print!("{}", line.origin()),
